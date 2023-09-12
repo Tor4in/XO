7422ff281b6a13241bfa846f0d389349fe66ee69
Віталік Тарчинець <tor4in@MacBook-Air-Vitalik.local>
2023-09-13 00:38:30 +0300

    deyli end

--- scrypt.js
+++ scrypt.js
@@ -9,8 +9,8 @@ const diadonale1 = document.querySelectorAll(".diadonale1");
 const diadonale2 = document.querySelectorAll(".diadonale2");
 
 let playerX = true;
-let win = false;
-  
+let final = document.querySelector(".final");
+
 let winCheck = (name) => {
   if (
     name[0].innerHTML == name[1].innerHTML &&
@@ -21,22 +21,24 @@ let winCheck = (name) => {
       name[1].classList.contains("X") &&
       name[2].classList.contains("X")
     ) {
-      win = !win;
-      console.log("X won");
+      final.classList.add("win");
+      playerX = true;
+      return true;
     } else if (
       name[0].classList.contains("O") &&
       name[1].classList.contains("O") &&
       name[2].classList.contains("O")
     ) {
-      win = !win;
-      console.log("O won");
+      final.classList.add("win");
+      playerX = true;
+      return true;
     }
   }
 };
 
 for (const m of move) {
   m.addEventListener("click", () => {
-    if (!m.classList.contains("active") && !win) {
+    if (!m.classList.contains("active") && !final.classList.contains("win")) {
       if (playerX) {
         m.classList.add("X");
       } else {
@@ -53,6 +55,63 @@ for (const m of move) {
       winCheck(line3);
       winCheck(diadonale1);
       winCheck(diadonale2);
+
+      if (winCheck(row1)) {
+        setTimeout(() => {
+          final.classList.add("row1Show");
+        }, 400);
+      }
+      if (winCheck(row2)) {
+        setTimeout(() => {
+          final.classList.add("row2Show");
+        }, 400);
+      }
+      if (winCheck(row3)) {
+        setTimeout(() => {
+          final.classList.add("row3Show");
+        }, 400);
+      }
+      if (winCheck(line1)) {
+        setTimeout(() => {
+          final.classList.add("line1Show");
+        }, 400);
+      }
+      if (winCheck(line2)) {
+        setTimeout(() => {
+          final.classList.add("line2Show");
+        }, 400);
+      }
+      if (winCheck(line3)) {
+        setTimeout(() => {
+          final.classList.add("line3Show");
+        }, 400);
+      }
+      if (winCheck(diadonale1)) {
+        setTimeout(() => {
+          final.classList.add("diadonale1Show");
+        }, 400);
+      }
+      if (winCheck(diadonale2)) {
+        setTimeout(() => {
+          final.classList.add("diadonale2Show");
+        }, 400);
+      }
     }
   });
 }
+let reload = document.querySelector(".reload");
+
+reload.addEventListener("click", () => {
+  for (const m of move) {
+    if (m.classList.contains("active")) {
+      m.classList.remove("active");
+      if (m.classList.contains("X")) {
+        m.classList.remove("X");
+      } else if (m.classList.contains("O")) {
+        m.classList.remove("O");
+      }
+    }
+    playerX = true;
+    final.classList = "final";
+  }
+});

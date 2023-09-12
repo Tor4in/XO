9bdc92b7c0acc5a68d5cdf61ef06351c1ce09800
Віталік Тарчинець <tor4in@MacBook-Air-Vitalik.local>
2023-09-12 21:36:54 +0300

    first commit

---
+++ scrypt.js
@@ -0,0 +1,58 @@
+const move = document.querySelectorAll(".move");
+const row1 = document.querySelectorAll(".row1");
+const row2 = document.querySelectorAll(".row2");
+const row3 = document.querySelectorAll(".row3");
+const line1 = document.querySelectorAll(".line1");
+const line2 = document.querySelectorAll(".line2");
+const line3 = document.querySelectorAll(".line3");
+const diadonale1 = document.querySelectorAll(".diadonale1");
+const diadonale2 = document.querySelectorAll(".diadonale2");
+
+let playerX = true;
+let win = false;
+
+let winCheck = (name) => {
+  if (
+    name[0].innerHTML == name[1].innerHTML &&
+    name[1].innerHTML == name[2].innerHTML
+  ) {
+    if (
+      name[0].classList.contains("X") &&
+      name[1].classList.contains("X") &&
+      name[2].classList.contains("X")
+    ) {
+      win = !win;
+      console.log("X won");
+    } else if (
+      name[0].classList.contains("O") &&
+      name[1].classList.contains("O") &&
+      name[2].classList.contains("O")
+    ) {
+      win = !win;
+      console.log("O won");
+    }
+  }
+};
+
+for (const m of move) {
+  m.addEventListener("click", () => {
+    if (!m.classList.contains("active") && !win) {
+      if (playerX) {
+        m.classList.add("X");
+      } else {
+        m.classList.add("O");
+      }
+
+      playerX = !playerX;
+      m.classList.add("active");
+      winCheck(row1);
+      winCheck(row2);
+      winCheck(row3);
+      winCheck(line1);
+      winCheck(line2);
+      winCheck(line3);
+      winCheck(diadonale1);
+      winCheck(diadonale2);
+    }
+  });
+}
